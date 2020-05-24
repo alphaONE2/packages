f37de2ba081206d819f862e99b568dbd2d9ae643
alphaONE2 <1904368+alphaONE2@users.noreply.github.com>
2020-05-24 03:14:11 -0500

    Fix syntax check

--- .github/workflows/check-syntax.ps1
+++ .github/workflows/check-syntax.ps1
@@ -1,10 +1,11 @@
 $exitCode = 0
 
-Get-ChildItem -Recurse -Filter '*.lua' |
+$root = Join-Path $PWD 'files'
+Get-ChildItem $root -Recurse -Filter '*.lua' |
   ForEach-Object {
-    Invoke-Expression -ErrorVariable err "&../tools/luajit -b `"$_`" -" *>$null
+    Invoke-Expression -ErrorVariable err "&tools/luajit -b `"$_`" -" *>$null
     if ($LASTEXITCODE -ne 0) {
-      $err = "$err".Replace($PWD, '.')
+      $err = "$err".Replace($root, '.')
       Write-Host -ForegroundColor Red $err
       $script:exitCode = 1
     }
