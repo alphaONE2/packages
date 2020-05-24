ff00de69a59b7ec9bf44891c9c42c69a323f9455
alphaONE2 <1904368+alphaONE2@users.noreply.github.com>
2020-05-24 03:50:57 -0500

    Fix

--- .github/workflows/check-syntax.ps1
+++ .github/workflows/check-syntax.ps1
@@ -1,12 +1,9 @@
 $exitCode = 0
 
-$dir =  "$(Get-Location)"
 Get-ChildItem -Recurse -Filter '*.lua' |
   ForEach-Object {
-    Invoke-Command {&.tools/luajit -b $_ -} -ErrorVariable err *>$null
+    &.tools/luajit -b $_ - >$null
     if ($LASTEXITCODE -ne 0) {
-      $err = "$err".Replace($dir, '.')
-      #Write-Host -ForegroundColor Red $err
       $script:exitCode = 1
     }
   }
