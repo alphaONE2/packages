62047b48e6addb0f0e602d2b141a8a4813965747
alphaONE2 <1904368+alphaONE2@users.noreply.github.com>
2020-05-24 03:33:56 -0500

    Attempt fix

--- .github/workflows/check-syntax.ps1
+++ .github/workflows/check-syntax.ps1
@@ -6,7 +6,7 @@ Get-ChildItem -Recurse -Filter '*.lua' |
     Invoke-Expression -ErrorVariable err "&.tools/luajit -b `"$_`" -" *>$null
     if ($LASTEXITCODE -ne 0) {
       $err = "$err".Replace($dir, '.')
-      Write-Host -ForegroundColor Red $err
+      #Write-Host -ForegroundColor Red $err
       $script:exitCode = 1
     }
   }
