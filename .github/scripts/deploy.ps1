ead1c8a8d88a9bcdfde18bd2892dfccbcdc2b372
alphaONE2 <1904368+alphaONE2@users.noreply.github.com>
2020-05-25 16:07:03 -0500

    Fix staging path

--- .github/scripts/deploy.ps1
+++ .github/scripts/deploy.ps1
@@ -3,7 +3,7 @@ $repo = ($env:GITHUB_REPOSITORY -Split '/')[1] ?? $env:GITHUB_REPOSITORY
     "rm -rf ~/staging/${repo}",
     "mkdir -p ~/staging/${repo}"
 ) -Join '&&')
-&scp -r './staging/*' "${env:SSH_USER}@${env:SSH_HOST}:~/staging/${repo}"
+&scp -r './.staging/*' "${env:SSH_USER}@${env:SSH_HOST}:~/staging/${repo}"
 &ssh "${env:SSH_USER}@${env:SSH_HOST}" (@(
     "mv -f ~/staging/${repo}/* ${env:FILES_PATH}/",
     "rm -r ~/staging/${repo}"
